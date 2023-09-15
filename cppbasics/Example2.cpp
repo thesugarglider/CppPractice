a27cd4f1f50256bad96a753f92d098be22ad3bcd
The Sugar Glider <thesugarglider7@gmail.com>
2023-09-15 09:00:56 +0530

    addition program

---
+++ cppbasics/Example2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout << "Enter two nos.\n";
+    cin >> a >> b;
+    a+=b;
+    cout << "Addition is " << a << endl;
+    return 0;
+}
