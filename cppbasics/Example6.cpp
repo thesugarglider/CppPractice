0d6ed70185e9a18076974cdcc3f8ff28d24b3ffd
pravinkumarosingh <pravinkumarosingh@gmail.com>
2023-09-17 18:26:25 +0530

    area of triangle program

---
+++ cppbasics/Example6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    const float constMultiplier = 0.5f;
+    int base, height;
+    base = 10;
+    height = 5;
+
+    cout << constMultiplier * base * height << "\n";
+
+    return 0;
+}
