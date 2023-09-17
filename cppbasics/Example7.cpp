e3379549814719dccbfc94bfb430aa51e7c70e53
pravinkumarosingh <pravinkumarosingh@gmail.com>
2023-09-17 18:30:01 +0530

    operator precedence example

---
+++ cppbasics/Example7.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=5;
+    int sum=0;
+    //sum of n natural numbers
+    sum = n*(n+1)/2;
+    cout << sum << "\n";
+    return 0;
+}
